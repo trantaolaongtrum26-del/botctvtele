dd2a1f8d48ccfd14f25f541facc599dccb353e3d
trantaolaongtrum26-del <trantaolaongtrum26@gmail.com>
2026-01-20 13:22:01 +0700

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from threading import Thread
+import logging
+
+# Tắt log của Flask để tránh rác màn hình
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
 
 app = Flask('')
 
 @app.route('/')
 def home():
-    return "I'm alive"
+    # CHỈ TRẢ VỀ DÒNG CHỮ NGẮN GỌN NÀY THÔI
+    return "I am alive!" 
 
 def run():
     app.run(host='0.0.0.0', port=8080)
